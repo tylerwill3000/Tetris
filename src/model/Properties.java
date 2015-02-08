48af242738419fd44958869183f5efb4a5adfc26
Tyler Williams <tylerwilliams3000@gmail.com>
2015-02-08 02:37:45 -0600

    Updated Properties.java

--- src/model/Properties.java
+++ src/model/Properties.java
@@ -11,59 +11,72 @@ import javax.swing.JOptionPane;
 
 import model.PieceFactory.PieceType;
 
-// Provides an interface to the game's properties
+/**
+ *  Provides an interface to the game's properties
+ * @author Tyler
+ */
 public class Properties {
 	
 	private static final String PROPERTIES_FILE_PATH = System.getProperty("user.home") + "/.tetrisconfig"; 
 	public static final java.util.Properties GAME_PROPERTIES = new java.util.Properties();
 	static { loadPropertiesFromDisk(); }
 	
+	// Centralize management of all property keys
+	private final static String DB_HOST_KEY = "db.host";
+	private final static String DB_NAME_KEY = "db.name";
+	private final static String DB_USER_KEY = "db.user";
+	private final static String DB_PASS_KEY = "db.pass";
+	private final static String PLAYER_SAVE_NAME_KEY = "player.save.name";
+	private final static String PIECE_BORDER_STYLE_KEY = "piece.border.style";
+	private final static String HIGH_SCORES_RECORD_KEY = "highscores.record.count";
+	private final static String HIGH_SCORES_DIFFICULTY_KEY = "highscores.difficulty";
+	
 	public static String getDBHostProperty() {
-		return GAME_PROPERTIES.getProperty("db.host");
+		return GAME_PROPERTIES.getProperty(DB_HOST_KEY);
 	}
 	
 	public static void setDBHostProperty(String host) {
-		GAME_PROPERTIES.setProperty("db.host", host);
+		GAME_PROPERTIES.setProperty(DB_HOST_KEY, host);
 	}
 	
 	public static String getDBNameProperty() {
-		return GAME_PROPERTIES.getProperty("db.name");
+		return GAME_PROPERTIES.getProperty(DB_NAME_KEY);
 	}
 	
 	public static void setDBNameProperty(String name) {
-		GAME_PROPERTIES.setProperty("db.name", name);
+		GAME_PROPERTIES.setProperty(DB_NAME_KEY, name);
 	}
 	
 	public static String getDBUserProperty() {
-		return GAME_PROPERTIES.getProperty("db.user");
+		return GAME_PROPERTIES.getProperty(DB_USER_KEY);
 	}
 	
 	public static void setDBUserProperty(String user) {
-		GAME_PROPERTIES.setProperty("db.user", user);
+		GAME_PROPERTIES.setProperty(DB_USER_KEY, user);
 	}
 	
 	public static String getDBPassProperty() {
-		return GAME_PROPERTIES.getProperty("db.pass");
+		return GAME_PROPERTIES.getProperty(DB_PASS_KEY);
 	}
 	
 	public static void setDBPassProperty(String pass) {
-		GAME_PROPERTIES.setProperty("db.pass", pass);
+		GAME_PROPERTIES.setProperty(DB_PASS_KEY, pass);
 	}
 	
 	public static String getPlayerSaveName() {
-		return GAME_PROPERTIES.getProperty("player.save.name");
+		return GAME_PROPERTIES.getProperty(PLAYER_SAVE_NAME_KEY);
 	}
 	
 	public static void setPlayerSaveName(String name) {
-		GAME_PROPERTIES.setProperty("player.save.name", name);
+		GAME_PROPERTIES.setProperty(PLAYER_SAVE_NAME_KEY, name);
 	}
 	
 	public static int getPieceBorderProperty() {
-		return Integer.parseInt(GAME_PROPERTIES.getProperty("piece.border.style"));
+		return Integer.parseInt(GAME_PROPERTIES.getProperty(PIECE_BORDER_STYLE_KEY));
 	}
 	
 	public static void setPieceBorderProperty(int pieceBorder) {
-		GAME_PROPERTIES.setProperty("piece.border.style", String.valueOf(pieceBorder));
+		GAME_PROPERTIES.setProperty(PIECE_BORDER_STYLE_KEY, String.valueOf(pieceBorder));
 	}
 	
 	public static boolean getActivePieceProperty(PieceType type) {
@@ -77,19 +90,19 @@ public class Properties {
 	}
 	
 	public static int getHighScoreRecordCount() {
-		return Integer.parseInt(GAME_PROPERTIES.getProperty("highscores.record.count"));
+		return Integer.parseInt(GAME_PROPERTIES.getProperty(HIGH_SCORES_RECORD_KEY));
 	}
 	
 	public static void setHighScoreRecordCount(int count) {
-		GAME_PROPERTIES.setProperty("highscores.record.count", String.valueOf(count));
+		GAME_PROPERTIES.setProperty(HIGH_SCORES_RECORD_KEY, String.valueOf(count));
 	}
 	
 	public static int getHighScoreDifficulty() {
-		return Integer.parseInt(GAME_PROPERTIES.getProperty("highscores.difficulty"));
+		return Integer.parseInt(GAME_PROPERTIES.getProperty(HIGH_SCORES_DIFFICULTY_KEY));
 	}
 	
 	public static void setHighScoresDifficulty(int diff) {
-		GAME_PROPERTIES.setProperty("highscores.difficulty", String.valueOf(diff));
+		GAME_PROPERTIES.setProperty(HIGH_SCORES_DIFFICULTY_KEY, String.valueOf(diff));
 	}
 	
 	/**
@@ -124,7 +137,7 @@ public class Properties {
 		setPlayerSaveName("player1");
 		setPieceBorderProperty(0);
 		
-		for (PieceType piece : PieceFactory.PieceType.getSpecialPieces()) {
+		for (PieceType piece : PieceType.getSpecialPieces()) {
 			setActivePieceProperty(piece, false);
 		}
 		
