e8b1a57f07bf6b0d50a8dc9d008e99cca16002af
Tyler Sharpe <Tyler.D.Sharpe@HealthPartners.com>
2020-07-18 23:32:35 -0500

    Remove unused method

--- src/main/java/com/github/tylersharpe/tetris/Rotation.java
+++ src/main/java/com/github/tylersharpe/tetris/Rotation.java
@@ -1,10 +1,3 @@
 package com.github.tylersharpe.tetris;
 
-public enum Rotation {
-  CLOCKWISE,
-  COUNTER_CLOCKWISE;
-
-  public Rotation reverse() {
-    return this == CLOCKWISE ? COUNTER_CLOCKWISE : CLOCKWISE;
-  }
-}
+public enum Rotation { CLOCKWISE, COUNTER_CLOCKWISE }
